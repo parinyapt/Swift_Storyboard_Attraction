116a0496e2b24ff49e6c47f8cf39e7bbf242e340
parinyapt <parinyapt99@gmail.com>
2022-10-01 22:00:27 +0700

    Initial Commit

---
+++ assignment5-attraction-UITableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment5-attraction-UITableView
+//
+//  Created by Parinya Termkasipanich on 1/10/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
